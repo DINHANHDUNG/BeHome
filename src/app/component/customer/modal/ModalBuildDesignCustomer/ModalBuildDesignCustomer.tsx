7f007784b7364617fad48a04fe9daee04bd7b604
DUNGITVN <dinhanhdung03011999@gmail.com>
2022-07-12 23:06:33 +0700

    update

---
+++ src/app/component/customer/modal/ModalBuildDesignCustomer/ModalBuildDesignCustomer.tsx
@@ -0,0 +1,261 @@
+import { Col, Modal, Pagination, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  getAllProductAdmin,
+  getProductSearchAdmin,
+} from "../../../../../features/Admin/productAdnim";
+import { productAdminStore } from "../../../../../use-selector";
+import {
+  Numberformat,
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../hooks";
+import FilterProductCombo from "../../../../page/admin/filterProductCombo/filterProductCombo";
+import Product from "../../product/product";
+import "./modalbui.css";
+interface propsModalBuildDesignCustomer {
+  visible: boolean;
+  toggle: () => void;
+  toggleSelect: (value: any) => void;
+  value: any;
+}
+function ModalBuildDesignCustomer(props: propsModalBuildDesignCustomer) {
+  const dispatch = useAppDispatch();
+  const [sort, setSort] = useState(0);
+  const [page, setPage] = useState(1);
+  const [notitem, setNotitem] = useState(10);
+  const product = useAppSelector(productAdminStore);
+  const [valueRangePrice, setValueRangePrice] = useState(null as any);
+  const [valueFirm, setValueFirm] = useState(null as any);
+  const [valueRank, setValueRank] = useState(null as any);
+  console.log(product);
+
+  useEffect(() => {
+    if (props.value.idCategory) {
+      dispatch(
+        getProductSearchAdmin({
+          id_category: props.value.idCategory,
+          id_rank: null,
+          id_manufacturer: null,
+          productKey: null,
+          minprice: null,
+          maxprice: null,
+          page: 1,
+          noitem: 10,
+          sort: sort,
+        })
+      );
+    }
+
+    setPage(1);
+    setNotitem(10);
+  }, [props.value.idCategory]);
+  return (
+    <Modal
+      title={"Chọn linh kiện"}
+      centered
+      visible={props.visible}
+      onOk={() => props.toggle()}
+      onCancel={() => props.toggle()}
+      //   okText={"Thêm"}
+      //   cancelText={"Hủy"}
+      width={1000}
+      footer={null}
+    >
+      <div className="row">
+        <div className="col-lg-9">
+          <div className="toolbox">
+            <div className="toolbox-left">
+              <div className="toolbox-sort">
+                <label htmlFor="sortby">Lọc:</label>
+                <div className="select-custom">
+                  <select
+                    name="sortby"
+                    id="sortby"
+                    className="form-control"
+                    onChange={(value) => {
+                      console.log(value.target.value);
+                      setSort(Number(value.target.value));
+                      setPage(1);
+                      setNotitem(10);
+                      dispatch(
+                        getProductSearchAdmin({
+                          id_category: props.value.idCategory,
+                          id_rank: valueRank,
+                          id_manufacturer: valueFirm,
+                          productKey: null,
+                          minprice: valueRangePrice ? valueRangePrice[0] : null,
+                          maxprice: valueRangePrice ? valueRangePrice[1] : null,
+                          page: 1,
+                          noitem: 10,
+                          sort: Number(value.target.value),
+                        })
+                      );
+                    }}
+                    // value={sort}
+                  >
+                    <option value={0} defaultChecked>
+                      Mới nhất
+                    </option>
+                    <option value={2}>Giảm dần</option>
+                    <option value={1}>Tăng dần</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div className="toolbox-right">
+              {product.listproduct?.length > 0 ? (
+                <Row gutter={[24, 24]}>
+                  <Col xl={24}>
+                    <Pagination
+                      style={{
+                        marginTop: "10px",
+                        float: "right",
+                        marginBottom: "10px",
+                      }}
+                      onChange={(page, pageSizeNew) => {
+                        console.log(page, pageSizeNew);
+                        setPage(page);
+                        dispatch(
+                          getProductSearchAdmin({
+                            id_category: props.value.idCategory,
+                            id_rank: valueRank,
+                            id_manufacturer: valueFirm,
+                            productKey: null,
+                            minprice: valueRangePrice
+                              ? valueRangePrice[0]
+                              : null,
+                            maxprice: valueRangePrice
+                              ? valueRangePrice[1]
+                              : null,
+                            page: page,
+                            noitem: pageSizeNew,
+                            sort: sort,
+                          })
+                        );
+                        if (pageSizeNew) {
+                          setNotitem(pageSizeNew);
+                        }
+                      }}
+                      pageSize={notitem}
+                      current={page}
+                      total={product.total}
+                    />
+                  </Col>
+                </Row>
+              ) : null}
+            </div>
+          </div>
+
+          <div className="products mb-3">
+            <div className="row">
+              {product.listproduct?.map((val, idx) => (
+                <div className="Modal-item-build" key={idx}>
+                  <div className="Modal-item-build-product">
+                    <figure className="Modal-item-build-product-media mr-2">
+                      <a href="#">
+                        <img
+                          style={{ width: "80px", height: "80px" }}
+                          src={
+                            "http://103.173.155.138:5500/images/" +
+                            val.images[0]?.imagename
+                          }
+                          alt="Product image"
+                        />
+                      </a>
+                    </figure>
+
+                    <h3
+                      className="product-title"
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "70%",
+                      }}
+                    >
+                      <a href="#" style={{ fontWeight: 500 }}>
+                        {val.name}
+                      </a>
+                      <span>{Numberformat(val.price)} đ</span>
+                    </h3>
+                  </div>
+
+                  <div className="Modal-qty-product-build">
+                    <span>
+                      <i
+                        className="fa-solid fa-square-plus mr-3"
+                        onClick={() => {
+                          props.toggleSelect({
+                            product: val,
+                            idBuild: props.value.selectedID,
+                          });
+                          props.toggle();
+                        }}
+                        style={{
+                          cursor: "pointer",
+                          color: "#39f",
+                          fontSize: "40px",
+                        }}
+                      ></i>
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <FilterProductCombo
+          toggleCleanAll={() => {
+            setValueRank(null);
+            setValueFirm(null);
+            setValueRangePrice(null);
+            setSort(0);
+            dispatch(
+              getProductSearchAdmin({
+                id_category: props.value.idCategory,
+                id_rank: null,
+                id_manufacturer: null,
+                productKey: null,
+                minprice: null,
+                maxprice: null,
+                page: 1,
+                noitem: 10,
+                sort: 0,
+              })
+            );
+
+            setPage(1);
+            setNotitem(10);
+          }}
+          toggleFilter={(value) => {
+            console.log(value);
+            setValueRank([value.rangePrice[0], value.rangePrice[1]]);
+            setValueFirm(value.rank);
+            setValueRangePrice(value.manufacturer);
+            dispatch(
+              getProductSearchAdmin({
+                id_category: props.value.idCategory,
+                id_rank: value.rank,
+                id_manufacturer: value.manufacturer,
+                productKey: null,
+                minprice: value.rangePrice[0],
+                maxprice: value.rangePrice[1],
+                page: 1,
+                noitem: 10,
+                sort: sort,
+              })
+            );
+            setPage(1);
+            setNotitem(10);
+          }}
+          type="PRODUCT"
+          id_category={props.value.idCategory}
+        />
+      </div>
+    </Modal>
+  );
+}
+
+export default ModalBuildDesignCustomer;
--- src/app/page/build/build.tsx
+++ src/app/page/build/build.tsx
@@ -17,6 +17,7 @@ import {
 } from "../../../features/Admin/buildAdmin";
 import { buildAdminStore } from "../../../use-selector";
 import ModalBuildDesign from "../../component/customer/modal/modalBuildDesign/modal-buildDesign";
+import ModalBuildDesignCustomer from "../../component/customer/modal/ModalBuildDesignCustomer/ModalBuildDesignCustomer";
 import { Numberformat, useAppDispatch, useAppSelector } from "../../hooks";
 import "./build.css";
 function BuildDesign() {
@@ -25,11 +26,16 @@ function BuildDesign() {
   const dispatch = useAppDispatch();
   const buildDesign = useAppSelector(buildAdminStore);
 
-  const [selected, setSelected] = useState([] as any);
+  const [selectedProduct, setSelectedProduct] = useState([
+    { listProduct: [], idBuild: 0 },
+  ] as any);
+
+  console.log(selectedProduct);
+
   const [selectedID, setSelectedID] = useState(1);
+  const [idCategory, setIdCategory] = useState(null as any);
   const [selectIndex, setSelectIndex] = useState(Number);
   const [visible, setVisible] = useState(false);
-  const [value, setValue] = useState({ id: 0, name: "", builddesigns: [] });
   console.log(buildDesign);
 
   useEffect(() => {
@@ -41,6 +47,11 @@ function BuildDesign() {
       setSelectedID(buildDesign.listBuild[0].id);
       setSelectIndex(0);
     }
+    var arrNew = [] as any;
+    buildDesign.listBuild.map((value) => {
+      arrNew.push({ listProduct: [], idBuild: value.id });
+    });
+    setSelectedProduct(arrNew);
   }, [buildDesign]);
 
   return (
@@ -57,6 +68,8 @@ function BuildDesign() {
                   : {}
               }
               onClick={() => {
+                console.log(1);
+
                 setSelectedID(value.id);
                 setSelectIndex(idx);
               }}
@@ -73,24 +86,89 @@ function BuildDesign() {
 
       {buildDesign.listBuild[selectIndex]?.builddesigns?.map((value, idx) => (
         <>
-          {" "}
+          {console.log(
+            selectedProduct.filter((val: any) => val.id != value.id)[0]
+              ?.listProduct, value
+          )}{" "}
           <hr />
           <div className="row" key={value.id}>
             <div className="col-3" style={{ borderRight: "1px solid" }}>
-              {idx + 1} . {value.category?.name}
+              {idx + 1} . {value.category?.name} 
+              {/* {value.id_category} */}
             </div>
-            <div className="col-9">
-              <div
-                className="product-details-action mb-0"
-                style={{ maxWidth: "200px" }}
-                onClick={() => {}}
-              >
-                <a className="btn-product btn">
-                  <span>Chọn {value.category?.name}</span>
-                </a>
+
+            {selectedProduct.filter((val: any) => val.id != value.id)[0]
+              ?.listProduct.id_category === value.id_category ? (
+              <div className="col-9">
+                <div className="item-build">
+                  <div className="item-build-product">
+                    <figure className="item-build-product-media mr-2">
+                      <a href="#">
+                        <img
+                          src={
+                            "http://103.173.155.138:5500/images/1a94a9c414ec472ab0627aed3258f994.jpg"
+                          }
+                          alt="Product image"
+                        />
+                      </a>
+                    </figure>
+
+                    <h3 className="product-title" style={{ paddingTop: "7px" }}>
+                      <a href="#" style={{ fontWeight: 500 }}>
+                        CPU Intel Core i9-12900KS CPU Intel Core i9-12900KS CPU
+                        Intel Core i9-12900KSCPU Intel Core i9-12900KS
+                      </a>
+                    </h3>
+                  </div>
+
+                  <div className="qty-product-build">
+                    <span>
+                      {Numberformat(200000)} x{" "}
+                      <InputNumber
+                        min={1}
+                        max={10}
+                        value={1}
+                        onChange={(val) => {}}
+                      />{" "}
+                      ={" "}
+                      <span className="mr-3" style={{ color: "red" }}>
+                        {Numberformat(300000)}
+                      </span>
+                      <i
+                        className="fa-solid fa-pen mr-3"
+                        style={{ cursor: "pointer", color: "#39f" }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-trash-can"
+                        style={{ cursor: "pointer", color: "red" }}
+                      ></i>
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="col-9">
+                <div
+                  className="product-details-action mb-0"
+                  style={{ maxWidth: "200px" }}
+                  onClick={() => {
+                    setVisible(true);
+                    setIdCategory(value.category?.id);
+                  }}
+                >
+                  <a className="btn-product btn">
+                    <span>Chọn {value.category?.name}</span>
+                  </a>
+                </div>
+              </div>
+            )}
           </div>
+          {/* <div className="row" key={idx}>
+            <div className="col-3" style={{ borderRight: "1px solid" }}>
+              1. Test
+            </div>
+            
+          </div> */}
         </>
       ))}
 
@@ -175,6 +253,40 @@ function BuildDesign() {
           <span>Thêm vào giỏ hàng</span>
         </a>
       </div>
+      {/* {visible ?? ( */}
+      <ModalBuildDesignCustomer
+        visible={visible}
+        toggleSelect={(value) => {
+          console.log(value);
+
+          let newArr = [] as any;
+
+          selectedProduct.map((val: any) => {
+            if (val.idBuild === value.idBuild) {
+              var check = val.listProduct?.findIndex(
+                (v: any, idx: any) => v.id_category === idCategory
+              );
+              if (check >= 0) {
+                newArr.push(val);
+              } else {
+                newArr.push({
+                  listProduct: [...val.listProduct, value.product],
+                  idBuild: val.idBuild,
+                });
+              }
+            } else {
+              newArr.push(val);
+            }
+          });
+
+          setSelectedProduct(newArr);
+        }}
+        toggle={() => {
+          setVisible(false);
+        }}
+        value={{ selectedID, idCategory }}
+      />
+      {/* )} */}
     </div>
   );
 }
